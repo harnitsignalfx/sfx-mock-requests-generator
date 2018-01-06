c20a56132caff4614b364bee835c87d8962ad547
Harnit Singh <harnit.singh@signalfx.com>
2018-01-05 17:15:39 -0800

    typos

--- mock-requests.py
+++ mock-requests.py
@@ -5,16 +5,17 @@ import os
 
 async def main():
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:	
-    	target = 'http://localhost:8901'
-	if 'TARGET_ADDRESS' in os.environ:
-	    target = os.environ['TARGET_ADDRESS']
-	print ('Sending data to ..',target)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        target = 'http://localhost:8901'
+        if 'TARGET_ADDRESS' in os.environ:
+            target = os.environ['TARGET_ADDRESS']
+
+        print ('Sending data to ..',target)
         loop = asyncio.get_event_loop()
         futures = [
             loop.run_in_executor(
-                executor, 
-                requests.get, 
+                executor,
+                requests.get,
                 target
             )
             for i in range(20)
